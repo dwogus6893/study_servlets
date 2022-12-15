740f6b8278a3dfbd0b26deac58b5f1055c9a52ae
dwogus6893 <dwogus6893@naver.com>
2022-12-15 09:16:32 +0900

    sss

--- src/main/java/com/dwogus6893/study_servlets/HelloWorldServlets.java
+++ src/main/java/com/dwogus6893/study_servlets/HelloWorldServlets.java
@@ -3,24 +3,28 @@ package com.dwogus6893.study_servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
+@WebServlet(urlPatterns = "/helloWorldServlets")
 public class HelloWorldServlets extends HttpServlet {
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-    //별칭 매칭
-    //http://localhost:8080/helloWorldServlets
-    //../java/com/yojulab/learn_servlets/HelloWorldServlets.class
-    
+    String message = "Hello WolrdServlets with message";
     PrintWriter printWriter =  response.getWriter();
-    
+
+    //http://localhost:8080/helloWorldServlets
+    //별칭 매칭
+    //../java/com/yojulab/learn_servlets/HelloWorldServlets.class
+        
     printWriter.println("<html lang='en'>");
     printWriter.println("<head>");
-    printWriter.println("<title>Hello WolrdServlets ! </title>");
+    printWriter.println("<title>"+message+"</title>");
     printWriter.println("</head>");
     printWriter.println("<body>");
-    printWriter.println("<div>Hello WolrdServlets ! </div>");
+    printWriter.println("<div>"+message+"</div>");
     printWriter.println("</body></html>");
 
     printWriter.println("<div></div>");
