745ee11e0a6c8546e55c8c77e75db4016b547b0a
dwogus6893 <dwogus6893@naver.com>
2022-12-15 11:00:15 +0900

    sss

---
+++ src/main/java/com/dwogus6893/study_servlets/FormsSelectsServlet.java
@@ -0,0 +1,65 @@
+package com.dwogus6893.study_servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/FormsSelectsServlet")
+public class FormsSelectsServlet extends HttpServlet{
+    //client에게 줄 부분 작성
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    //PrintWriter는 client에게 값을 보내기위한 준비작업
+    PrintWriter printWriter = response.getWriter();
+
+    //기본부분
+    String title = "Select";
+    printWriter.println("<html lang='en'>");    
+    printWriter.println("<head>");
+    printWriter.println("<title>"+title+"</title>");
+    printWriter.println("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' rel='stylesheet'");
+    printWriter.println("integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65' crossorigin='anonymous'>");
+    printWriter.println("<link rel='stylesheet' href='./css/commons.css'>");
+    printWriter.println("</head>");
+
+    //body
+    printWriter.println("<body>");
+    printWriter.println("<div class='fs-3'>form selects</div>"); 
+    printWriter.println("<div>"); 
+    printWriter.println("<label for='' class='form-label'>select multiple</label>"); 
+    printWriter.println("<select class='form-select' aria-label='' multiple>"); 
+    printWriter.println("<option selected>Open this select menu</option>"); 
+    printWriter.println("<option value=M01>One</option>"); 
+    printWriter.println("<option value=M02 selected>Two</option>"); 
+    printWriter.println("<option value=M03>Three</option>"); 
+    printWriter.println("<option value=M04 selected>Four</option>"); 
+    printWriter.println("<option value=M01>One</option>"); 
+    printWriter.println("<option value=M02 selected>Two</option>"); 
+    printWriter.println("<option value=M03>Three</option>"); 
+    printWriter.println("<option value=M04 selected>Four</option>"); 
+    printWriter.println("</select>"); 
+    printWriter.println("</div>"); 
+
+    printWriter.println("<div>"); 
+    printWriter.println("<label for='' class='form-label'>select </label>"); 
+    printWriter.println("<select class='form-select' aria-label=''>"); 
+    printWriter.println("<option selected>Open this select menu</option>"); 
+    printWriter.println("<option value=M01>One</option>"); 
+    printWriter.println("<option value=M02 selected>Two</option>"); 
+    printWriter.println("<option value=M03>Three</option>"); 
+    printWriter.println("</select>"); 
+    printWriter.println("</div>"); 
+
+    //기본 부분
+    printWriter.println("<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'");
+    printWriter.println("integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4'");
+    printWriter.println("crossorigin='anonymous'></script>");
+    printWriter.println("</body>");       
+    printWriter.println("</html>");
+    //자원 반납
+    printWriter.close();
+    } 
+}
--- src/main/java/com/dwogus6893/study_servlets/HelloWorldServlets.java
+++ src/main/java/com/dwogus6893/study_servlets/HelloWorldServlets.java
@@ -8,15 +8,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
+//웹서버가 어떤 것을 실행해야하는지 알려주는 경로다
 @WebServlet(urlPatterns = "/helloWorldServlets")
 public class HelloWorldServlets extends HttpServlet {
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
     String message = "Hello WolrdServlets with message";
     PrintWriter printWriter =  response.getWriter();
 
-    //http://localhost:8080/helloWorldServlets
-    //별칭 매칭
+    // http://localhost:8080/helloWorldServlets
+    // 별칭 매칭 하는 이유는 ?
+    // 자바 클래스가 외부가 노출되는 경로를 다 적기엔 불편하다, 그래서 매칭되는게 필요하다.
     //../java/com/yojulab/learn_servlets/HelloWorldServlets.class
         
     printWriter.println("<html lang='en'>");
@@ -25,7 +26,8 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
     printWriter.println("</head>");
     printWriter.println("<body>");
     printWriter.println("<div>"+message+"</div>");
-    printWriter.println("</body></html>");
+    printWriter.println("</body>");
+    printWriter.println("</html>");
 
     printWriter.println("<div></div>");
     printWriter.close();    
