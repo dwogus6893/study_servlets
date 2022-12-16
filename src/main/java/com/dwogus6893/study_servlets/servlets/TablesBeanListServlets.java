6f64286d63e5fba39330112cb1887878b196fde9
dwogus6893 <dwogus6893@naver.com>
2022-12-16 11:13:35 +0900

    ssdsds

--- src/main/java/com/dwogus6893/study_servlets/servlets/TablesBeanListServlets.java
+++ src/main/java/com/dwogus6893/study_servlets/servlets/TablesBeanListServlets.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.dwogus6893.study_servlets.DatasInfo;
+import com.dwogus6893.study_servlets.beans.MemberBean;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,56 +14,82 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/tablesListServlets")
-public class TablesBeanListServlets extends HttpServlet{
+@WebServlet(urlPatterns = "/tablesBeanListServlets")
+public class TablesBeanListServlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException{
-        response.setContentType("text/html; charset=UTF-8");
-        // 자바내용 import하기
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8"); // 응답을 보낼 때 한글이 깨지지 않게 해주는 것. 백엔드를 위한 것
+
+        // 여기서부터 액션을 취하는 부분
         DatasInfo datasInfo = new DatasInfo();
-        ArrayList<String> tablesListWithString = datasInfo.getTablesListWithString();
-        // 자바내용 import하기 끝
+        MemberBean memberBean = datasInfo.getDataWithMemberBean();
+        HashMap<String, Object> bundlesData = datasInfo.getBundleData();
+
+        System.out.println(memberBean.getFirstName() + " " + memberBean.getSecondName()
+                + " " + memberBean.getHandleName());
+
+        // 여기까지
+
+        // 여기서부터는 Display영역
         PrintWriter printWriter = response.getWriter();
-
-
         HashMap<String, String> searchForm = datasInfo.getSearchFormData();
+
         printWriter.println("<html lang='en'>");
         printWriter.println("<head>");
-        printWriter.println("<title>"+searchForm.get("search_key")+"</title>");
-        printWriter.println("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css' rel='stylesheet' ");
-        printWriter.println("integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65' crossorigin='anonymous'>");
-        printWriter.println("<link rel='stylesheet' href='./css/commons.css'> ");
+        printWriter.println("<meta charset='UTF-8'>");
+        printWriter.println("<title>" + searchForm.get("search_key") + "</title>");
+        printWriter.println("<link");
+        printWriter.println("href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'");
+        printWriter.println("rel='stylesheet'");
+        printWriter.println("integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65'");
+        printWriter.println("crossorigin='anonymous'");
+        printWriter.println("/>");
+        printWriter.println("<link rel='stylesheet' href='./css/commons.css' />");
         printWriter.println("</head>");
         printWriter.println("<body>");
         printWriter.println("<div class='container'>");
-        printWriter.println("<div class='fs-3'>Tables with Bean</div>");
+        printWriter.println("<div class='fs-3'>Tables With Bean</div>");
+        // printWriter.println("<div class='fs-4'>" + memberBean.getFirstName() + " " +
+        // memberBean.getSecondName()
+        // + " " + memberBean.getHandleName() + "</div>");
+
+        MemberBean memberBean2 = (MemberBean) bundlesData.get("dataWithMemberBean");
+        printWriter.println("<div class='fs-4'>" + memberBean2.getFirstName() + " " + memberBean2.getSecondName()
+                + " " + memberBean2.getHandleName() + "</div>");
+
         printWriter.println("<table class='table'>");
         printWriter.println("<thead>");
         printWriter.println("<tr>");
         printWriter.println("<th scope=>#</th>");
-        //printWriter.println("<th scope=>First</th>");
-        //printWriter.println("<th scope=>Last</th>");
+        // printWriter.println("<th scope=>First</th>");
+        // printWriter.println("<th scope=>Last</th>");
         printWriter.println("<th scope=>Handle</th>");
-        printWriter.println("   </tr>");
+        printWriter.println("</tr>");
         printWriter.println("</thead>");
         printWriter.println("<tbody>");
-        
-        for(int i=0; i<tablesListWithString.size();i++){
-            printWriter.println("   <tr>");
-            printWriter.println("       <th scope=>"+(i+1)+"</th>");
-            String handle = tablesListWithString.get(i);
-            printWriter.println("       <td>"+handle+"</td>");
-            printWriter.println("   </tr>");
-        }
-        printWriter.println("       </tbody>");
-        printWriter.println("   </table>");
-        printWriter.println("   </div>");
-        printWriter.println("<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' ");
-        printWriter.println("integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4' ");
-        printWriter.println("crossorigin='anonymous'></script>");
+        // for (int i = 0; i < tablesListWithString.size(); i++) {
+        // printWriter.println("<tr>");
+        // printWriter.println("<th scope=>" + (i + 1) + "</th>");
+        // // printWriter.println("<td>Mark</td>");
+        // // printWriter.println("<td>Otto</td>");
+        // String handle = tablesListWithString.get(i);
+        // printWriter.println("<td>" + handle + "</td>");
+        // printWriter.println("</tr>");
+        // }
+
+        printWriter.println("</tbody>");
+        printWriter.println("</table>");
+        printWriter.println("</div>");
+
+        printWriter.println("<script");
+        printWriter.println("src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'");
+        printWriter.println("integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4'");
+        printWriter.println("crossorigin='anonymous'");
+        printWriter.println("></script>");
         printWriter.println("</body>");
         printWriter.println("</html>");
-    printWriter.close();
-}
+
+        printWriter.close();
+    }
 }
