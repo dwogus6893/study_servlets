50fbe8d8fa713357d9e0e9bd7736b3704e5b5033
dwogus6893 <dwogus6893@naver.com>
2022-12-26 11:07:47 +0900

    6

--- src/main/java/com/dwogus6893/study_servlets/dao/PollWithDB.java
+++ src/main/java/com/dwogus6893/study_servlets/dao/PollWithDB.java
@@ -3,17 +3,34 @@ package com.dwogus6893.study_servlets.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PollWithDB {
-    public void getAnswer(String questionsUid){
-        // SELECT ANSWERS.QUESTIONS_UID, EXAMPLE_LIST.ORDERS, EXAMPLE_LIST.EXAMPLE 
-        // FROM ANSWERS 
-	    // INNER JOIN EXAMPLE_LIST 
-	    // ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID 
-	    // WHERE QUESTIONS_UID = 'Q1'
-        // ORDER BY QUESTIONS_UID;
-        return 0;
+    public ArrayList<HashMap> getAnswerList(String questionsUid) throws SQLException{
+       
+        Commons commons = new Commons();
+        Statement statement = commons.getStatement();
+        String query = "SELECT ANSWERS.QUESTIONS_UID, EXAMPLE_LIST.ORDERS, EXAMPLE_LIST.EXAMPLE "+
+                        "FROM ANSWERS "+
+                        "INNER JOIN EXAMPLE_LIST "+
+                        "ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID "+
+                        "WHERE QUESTIONS_UID = '"+questionsUid+"' "+
+                        "ORDER BY QUESTIONS_UID";
+        ResultSet resultSet = statement.executeQuery(query); //DB에 던짐;
+       
+        
+        ArrayList<HashMap> answer_list = new ArrayList<HashMap>();
+        while(resultSet.next()){
+            HashMap<String, Object> answer = new HashMap<String,Object>();
+            answer.put("QUESTIONS_UID",resultSet.getString("QUESTIONS_UID"));
+            answer.put("ORDERS",resultSet.getInt("ORDERS"));
+            answer.put("EXAMPLE", resultSet.getString("EXAMPLE"));
+         
+            answer_list.add(answer);
+        }        
+        return answer_list; //펑션의 귀약때문에 오류떠서 public void에 ...
+                            // void를 ->ArrayList<HashMap>로
 
     }
     public HashMap<String, Object> getQuestion(String questionsUid) throws SQLException{
--- src/main/java/com/dwogus6893/study_servlets/servlets/PollDetailServlets.java
+++ src/main/java/com/dwogus6893/study_servlets/servlets/PollDetailServlets.java
@@ -16,7 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/polls/PollServlet")
-public class DetailServlets extends HttpServlet {
+public class PollDetailServlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // input type
